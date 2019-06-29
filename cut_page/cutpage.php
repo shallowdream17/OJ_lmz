c1cd4c3a08aa46d1d9d7e55f8072f3a796eaecea
LiuMuZhuo <1007924005@qq.com>
2019-06-29 11:40:13 +0800

    _1 start version

---
+++ cut_page/cutpage.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>登录</title>
+</head>
+<body >
+<img style="position:absolute;left:0px;top:0px;width:100%;height:100%;z-Index:-1; border=none; solid blue" src="https://photo.16pic.com/00/55/40/16pic_5540821_b.jpg"  />
+
+</body>
+</html>
+<?php
+header("Content-Type: text/html;charset=utf-8");
+//数据库服务器
+define('DB_HOST', 'localhost');
+//数据库用户名
+define('DB_USER', 'root');
+//数据库密码
+define('DB_PWD', 'root');
+//库名
+define('DB_NAME', 'user_login');
+//字符集
+define('DB_CHARSET', 'utf8');
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PWD, DB_NAME);
+if (mysqli_errno($conn)) {
+    mysqli_error($conn);
+    exit;
+}
+mysqli_set_charset($conn, DB_CHARSET);
+
+
+$count_sql = 'select count(username) as c from users';
+
+$result = mysqli_query($conn, $count_sql);
+
+$data = mysqli_fetch_assoc($result);
+
+//得到总的用户数
+$count = $data['c'];
+
+$page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+
+//每页显示数
+
+$num = 5;
+
+//这是总页数
+$total = ceil($count / $num);
+
+if ($page <= 1) {
+    $page = 1;
+}
+
+if ($page >= $total) {
+    $page = $total;
+}
+
+
+$offset = ($page - 1) * $num;
+
+$sql = "select username,password from users order by username desc limit $offset , $num";
+
+$result = mysqli_query($conn, $sql);
+
+if ($result && mysqli_num_rows($result)) {
+
+    //存在数据则循环将数据显示出来
+    echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";
+    echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";
+    echo '<table border="1" cellpadding="3" cellspacing="0" style="width: 60%;margin:auto">';
+
+    while ($row = mysqli_fetch_assoc($result)) {
+
+        echo '<tr>';
+        echo '<td alion="center" valign="center">' . $row['username'] . '</td>';
+        echo '<td alion="center" valign="center">' . $row['password'] . '</td>';
+        echo '</tr>';
+    }
+
+    echo '<tr><td colspan="5"><a href="cutpage_2.php?page=1">首页</a>  <a href="cutpage_2.php?page=' . ($page - 1) . '">上一页</a>   <a href="cutpage_2.php?page=' . ($page + 1) . '">下一页</a>  <a href="cutpage_2.php?page=' . $total . '">尾页</a>  当前是第 ' . $page . '页  共' . $total . '页 </td></tr>';
+
+    echo '</table>';
+
+} else {
+    echo '没有数据';
+}
+
+mysqli_close($conn);
